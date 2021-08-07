745ad397151c7da31a40babe49fa0200d83913ad
Hasan <hasanozdemir1@trakya.edu.tr>
2021-08-07 18:12:58 +0300

    Download mp3 from music is DONE

--- src/root_path/welcome_selenium.java
+++ src/root_path/welcome_selenium.java
@@ -12,17 +12,17 @@ public class welcome_selenium {
         System.out.println("Welcome to Selenium Project");
         System.setProperty("webdriver.chrome.driver","C:\\Users\\bear_s_computer\\Desktop\\APPS\\Browser\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.get("https://mp3-youtube.download/tr/faster-online-convert");
+        driver.get("https://ytmp3.cc/downloader/");
         Thread.sleep(500);
-        WebElement link=driver.findElement(By.xpath("/html/body/main/section[2]/div/article/div/div/input"));
+        WebElement link_box=driver.findElement(By.id("input"));
         Thread.sleep(500);
-        link.sendKeys("https://www.youtube.com/watch?v=eYGOFfWIb9c");
+        link_box.sendKeys("https://www.youtube.com/watch?v=eYGOFfWIb9c");
         Thread.sleep(500);
-        WebElement convert_btn=driver.findElement(By.xpath("//*[@id=\"app\"]/div/article/div/div/div/button"));
+        WebElement convert_btn=driver.findElement(By.id("submit"));
         Thread.sleep(500);
         convert_btn.click();
         Thread.sleep(15000);
-        WebElement download_btn=driver.findElement(By.xpath("//*[@id=\"app\"]/div/article/div/p[2]/button"));
+        WebElement download_btn=driver.findElement(By.xpath("//*[@id=\"buttons\"]/a[1]"));
         download_btn.click();
         //driver.close();
     }
