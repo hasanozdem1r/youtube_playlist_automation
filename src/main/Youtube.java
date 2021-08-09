85cc7aa56e8a8e658372cdec669560dcc37eed08
Hasan <hasanozdemir1@trakya.edu.tr>
2021-08-09 21:38:53 +0300

    @download_mp3 function overwritten versions added.

--- src/main/Youtube.java
+++ src/main/Youtube.java
@@ -24,11 +24,11 @@ public class Youtube {
         @param mp3_link <String> : link of Youtube's video
         return void
         */
-        this.driver.get(mp3_link);
+        this.driver.get("https://ytmp3.cc/downloader/");
         Thread.sleep(300);
         WebElement link_box=this.driver.findElement(By.id("input"));
         Thread.sleep(300);
-        link_box.sendKeys("https://www.youtube.com/watch?v=eYGOFfWIb9c");
+        link_box.sendKeys(mp3_link);
         Thread.sleep(300);
         WebElement convert_btn=this.driver.findElement(By.id("submit"));
         Thread.sleep(300);
@@ -41,6 +41,28 @@ public class Youtube {
         System.out.println("Successful");
     }
 
+    //download mp3 overwritten method
+    public void download_mp3(List<String> mp3_link) throws InterruptedException{
+        this.driver.get("https://ytmp3.cc/downloader/");
+        for (String link:mp3_link){
+            Thread.sleep(1000);
+            WebElement link_box=this.driver.findElement(By.id("input"));
+            Thread.sleep(1000);
+            link_box.sendKeys(link);
+            Thread.sleep(1000);
+            WebElement convert_btn=this.driver.findElement(By.id("submit"));
+            Thread.sleep(1000);
+            convert_btn.click();
+            Thread.sleep(1000);
+            WebElement download_btn=this.driver.findElement(By.xpath("//*[@id=\"buttons\"]/a[1]"));
+            Thread.sleep(1000);
+            download_btn.click();
+            Thread.sleep(1000);
+            this.driver.get("https://ytmp3.cc/downloader/");
+       }
+    }
+
+    //get links of playlist
     public List<String> get_playlist_links(String playlist_link) throws InterruptedException{
         /*
         @param playlist_link <String> : link of Youtube's playlist
@@ -60,5 +82,6 @@ public class Youtube {
         System.out.println("FINISHED");
         return mp3_links;
     }
+
 }
 
--- src/main/main.java
+++ src/main/main.java
@@ -1,16 +1,13 @@
 package main;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.ArrayList;
+import java.util.List;
 
-
+//test playlist >> https://www.youtube.com/playlist?list=PLYa_HsGoCZnUk5e1K1BKeTUCIL8PBUWl7
+//test file for 1 music >> https://www.youtube.com/watch?v=rqbTuICtDzE&list=WL&index=142
 public class main {
     public static void main(String[] args) throws InterruptedException {
         Youtube obj=new Youtube();
-        //obj.download_mp3("https://ytmp3.cc/downloader/");
-        obj.get_playlist_links("https://www.youtube.com/playlist?list=PLoLjITz0dx2JUvvkPxjphRVEDZ3Oip5UL");
     }
 }
 
